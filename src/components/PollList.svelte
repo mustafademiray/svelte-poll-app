06c0cd164d6f9999a30cf833d3c56ab1bc02183f
Mustafa Demiray <mustafademiray@protonmail.com>
2021-08-28 20:13:59 +0200

    Add files via upload

---
+++ src/components/PollList.svelte
@@ -0,0 +1,35 @@
+<script>
+    //import { onMount, onDestroy } from 'svelte';
+    import { fade, slide, scale } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
+    import PollStore from '../stores/PollStore';
+    import PollDetails from './PollDetails.svelte';
+
+    //export let polls = [];
+    
+    //const unsub = PollStore.subscribe(data => {
+    //    polls = data;
+    //});
+
+    //onDestroy(() => {
+    //    console.log('component destroyed');
+    //    unsub();
+    //});
+</script>
+
+<div class="poll-list">
+    {#each $PollStore as poll (poll.id)}
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
+            <PollDetails {poll} />
+        </div>
+    {/each}
+
+</div>
+
+<style>
+    .poll-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
